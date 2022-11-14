08888b55a64db2e9a02fbad4163808d5d7f2aa0e
Matt Geller <mattg92@gmail.com>
2022-11-13 22:14:55 -0800

    Refactor to many-to-many relationship between users and games (unfinished)

---
+++ database/migrations/2022_11_14_054534_create_user_game_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Game;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_game', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Game::class)->constrained()->cascadeOnDelete();
+            $table->primary(['user_id', 'game_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_user');
+    }
+};
